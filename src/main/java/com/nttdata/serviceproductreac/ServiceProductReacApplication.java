d1bcd26aaa512c7521f973f29d5ba2e7fd537d26
darvera <darvera@protonmail.com>
2022-09-19 21:55:55 -0500

    first commit

---
+++ src/main/java/com/nttdata/serviceproductreac/ServiceProductReacApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.serviceproductreac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProductReacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProductReacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/serviceproductreac/ServiceProductReacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.serviceproductreac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProductReacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
